63273f43e36b51b6762e5e2e47d9a4166957b9e5
Ümmühan Kurt <63405463+ummuhankurt@users.noreply.github.com>
2022-12-15 17:45:34 +0300

    .

--- WebApiDemo/CustomMiddlewares/AuthenticationMiddleware.cs
+++ WebApiDemo/CustomMiddlewares/AuthenticationMiddleware.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace WebApiDemo.CustomMiddlewares
@@ -15,8 +17,46 @@ namespace WebApiDemo.CustomMiddlewares
         }
         public async Task Invoke(HttpContext context)
         {
-            var autHeader = context.Request.Headers["Authorization"];
-            await _next(context); 
+            string autHeader = context.Request.Headers["Authorization"];
+
+            //authenticationTürü boşluk( ) kullanıcıAdı:sifre
+            //basic ummuhan:12345
+            // StringComparison.OrdinalIgnoreCase -> harf duyarını devre dışı bırakır.
+            if (autHeader != null && autHeader.StartsWith("basic", StringComparison.OrdinalIgnoreCase)) //Headeri var mı? ve basicle başlıyor mu?
+            {
+                var token = autHeader.Substring(6).Trim();
+                var credentialString = "";
+                try
+                {
+                    credentialString = Encoding.UTF8.GetString(Convert.FromBase64String(token));
+                }
+                catch
+                {
+
+                    context.Response.StatusCode = 500;
+
+                }
+
+
+                var credantials = credentialString.Split(':');
+                if (credantials[0] == "ummuhan" && credantials[1] == "12345")
+                {
+                    var claims = new[]
+                    {
+                        new Claim("name",credantials[0]),
+                        new Claim(ClaimTypes.Role,"Admin")
+                    };
+                    var identity = new ClaimsIdentity(claims, "basic");
+                    context.User = new ClaimsPrincipal(identity);
+                }
+            }
+
+            else
+            {
+                context.Response.StatusCode = 401;
+            }
+
+            await _next(context);
         }
     }
 }
